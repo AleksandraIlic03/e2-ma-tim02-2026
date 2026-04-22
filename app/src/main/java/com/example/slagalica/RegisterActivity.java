ce386771f6d09edf85aa1c7386aaa26629fa14f5
Vasilisa Petrovic <vasilisapetrovic@icloud.com>
2026-04-22 16:23:23 +0200

    login and registration screens with navigation

---
+++ app/src/main/java/com/example/slagalica/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.slagalica;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView tvLogin = findViewById(R.id.tvLogin);
+        tvLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+    }
+}
